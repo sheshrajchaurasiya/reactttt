330d1ab37e68efe9bba5dc693fb5ccbc842b3afb
sheshrajchaurasiya <120588700+sheshrajchaurasiya@users.noreply.github.com>
2023-05-15 01:03:22 +0530

    code added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Calculator from './Components/Calculator'
+
+
+
+const App=()=>{
+return (
+  <div>
+    <h1>React Calculator</h1>
+    <Calculator/>
+
+  </div>
+)
+}
+
+
+
+
+
+export default App
